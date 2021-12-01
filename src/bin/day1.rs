27f70b01afa270494572f275cdccd225c7b191bd
Athena Yao <athena@discordapp.com>
2021-11-30 21:59:00 -0800

    [day 1] part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,5 @@
+use std::vec::Vec;
+
 fn count_increases(input: &str) -> u32 {
     let mut previous_number = u32::MAX;
     let mut num_increase = 0;
@@ -11,9 +13,28 @@ fn count_increases(input: &str) -> u32 {
     num_increase
 }
 
+fn count_window_increases(input: &str) -> u32 {
+    let lines: Vec<u32> = input.split("\n").map(|x| x.parse().unwrap()).collect();
+    let mut previous_sum = u32::MAX;
+    let mut num_increase = 0;
+
+    const WINDOW_SIZE: usize = 3;
+    for (i, line) in lines.iter().enumerate() {
+        if i + 1 < WINDOW_SIZE {
+            continue;
+        }
+        let sum = lines[i] + lines[i-1] + lines[i-2];
+        if sum > previous_sum {
+            num_increase += 1;
+        }
+        previous_sum = sum;
+    }
+    num_increase
+}
+
 fn main() {
     let input = include_str!("day1.txt");
-    let answer = count_increases(input);
+    let answer = count_window_increases(input);
     println!("ANSWER: {}", answer)
 }
 
@@ -24,5 +45,11 @@ mod tests {
     fn test_count_increases() {
         let input = include_str!("day1-example.txt");
         assert_eq!(count_increases(input), 7);
+    }
+
+    #[test]
+    fn test_count_window_increases() {
+        let input = include_str!("day1-example.txt");
+        assert_eq!(count_window_increases(input), 5);
     }
 }

0868fcf971b88bbb99426f4624cb3a1e82df258b
Athena Yao <athena@discordapp.com>
2021-12-01 21:32:34 -0800

    [template] More generic part 1 / part 2

--- src/bin/_day.rs
+++ src/bin/_day.rs
@@ -1,11 +1,15 @@
-fn do_something(input: &str) -> u32 {
+fn part_one(input: &str) -> u32 {
+    0
+}
+
+fn part_two(input: &str) -> u32 {
     0
 }
 
 fn main() {
     let input = include_str!("_day.txt");
-    let answer = do_something(input);
-    println!("ANSWER: {}", answer)
+    println!("Part 1: {}", part_one(input));
+    println!("Part 2: {}", part_two(input));
 }
 
 #[cfg(test)]
@@ -13,8 +17,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_do_something() {
-        assert_eq!(do_something(""), 0);
-        assert_eq!(do_something(""), 0);
+    fn test_part_one() {
+        let input = include_str!("_day-example.txt");
+        assert_eq!(part_one(input), 0);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = include_str!("_day-example.txt");
+        assert_eq!(part_two(input), 0);
     }
 }

1499b10b0e188f210f2e53b114c3a7a43db189b9
Athena Yao <athena@discordapp.com>
2021-12-01 21:17:45 -0800

    [day 2] Part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -19,9 +19,35 @@ fn calculate_depth(input: &str) -> u32 {
     horizontal * depth
 }
 
+fn calculate_depth_with_aim(input: &str) -> u32 {
+    let lines: Vec<_> = input.split("\n").collect();
+    let mut horizontal = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+
+    for &line in &lines {
+        let split: Vec<_> = line.split(" ").collect();
+        let direction = split[0];
+        let number: u32 = split[1].parse().unwrap();
+        match direction {
+            "forward" => {
+                horizontal += number;
+                depth += aim * number
+
+            },
+            "down" => aim += number,
+            "up" => aim -= number,
+            _ => {}
+        }
+
+    }
+    horizontal * depth
+}
+
+
 fn main() {
     let input = include_str!("day2.txt");
-    let answer = calculate_depth(input);
+    let answer = calculate_depth_with_aim(input);
     println!("ANSWER: {}", answer)
 }
 
@@ -33,5 +59,11 @@ mod tests {
     fn test_calculate_depth() {
         let input = include_str!("day2-example.txt");
         assert_eq!(calculate_depth(input), 150);
+    }
+
+    #[test]
+    fn test_calculate_depth_with_aim() {
+        let input = include_str!("day2-example.txt");
+        assert_eq!(calculate_depth_with_aim(input), 900);
     }
 }

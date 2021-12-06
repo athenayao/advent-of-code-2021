4693128382e4ad800d9a59708c69723b2e0bd0e8
Athena Yao <athena@discordapp.com>
2021-12-05 21:43:02 -0800

    [day 6] Part 1

---
+++ src/bin/day6.rs
@@ -0,0 +1,75 @@
+#[derive(Debug)]
+struct LanternFish {
+    timer: u32,
+}
+impl LanternFish {
+    fn tick(&mut self) -> bool {
+        let old_timer = self.timer;
+
+        self.timer = match &self.timer {
+            0 => 6,
+            x => x - 1,
+        };
+
+        if old_timer == 0 {
+            return true;
+        }
+        false
+    }
+}
+impl std::fmt::Display for LanternFish {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.timer)
+    }
+}
+
+fn part_one(input: &str, days: u32) -> u32 {
+    let mut school = Vec::new();
+    for fish in input.split(",") {
+        school.push(LanternFish {
+            timer: fish.parse().unwrap(),
+        })
+    }
+
+    for day in 1..=days {
+        let mut to_spawn = 0;
+        for fish in &mut school {
+            let should_spawn = fish.tick();
+            if should_spawn {
+                to_spawn += 1;
+            }
+        }
+        for _ in 0..to_spawn {
+            school.push(LanternFish { timer: 8 });
+        }
+    }
+    school.len() as u32
+}
+
+fn part_two(input: &str) -> u32 {
+    0
+}
+
+fn main() {
+    let input = include_str!("day6.txt");
+    println!("Part 1: {}", part_one(input, 80));
+    println!("Part 2: {}", part_two(input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = include_str!("day6-example.txt");
+        assert_eq!(part_one(input, 18), 26);
+        assert_eq!(part_one(input, 80), 5934);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = include_str!("day6-example.txt");
+        assert_eq!(part_two(input), 0);
+    }
+}

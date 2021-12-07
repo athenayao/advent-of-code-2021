95de44c6ce6e71032737c351e8147f6a56e2543b
Athena Yao <athena@discordapp.com>
2021-12-06 22:12:14 -0800

    cleanup rust

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,23 +1,3 @@
-fn find_min(vec: &Vec<i32>) -> i32 {
-    let mut min = &vec[0];
-    for x in vec.iter() {
-        if x < min {
-            min = x;
-        }
-    }
-    *min
-}
-
-fn find_max(vec: &Vec<i32>) -> i32 {
-    let mut max = &vec[0];
-    for x in vec.iter() {
-        if x > max {
-            max = x;
-        }
-    }
-    *max
-}
-
 fn sum_distance(vec: &Vec<i32>, position: i32) -> i32 {
     let mut total = 0;
     for x in vec.iter() {
@@ -29,8 +9,8 @@ fn sum_distance(vec: &Vec<i32>, position: i32) -> i32 {
 
 fn part_one(input: &str) -> i32 {
     let crabs: Vec<i32> = input.split(",").map(|n| n.parse().unwrap()).collect();
-    let min_range = find_min(&crabs);
-    let max_range = find_max(&crabs);
+    let min_range = *crabs.iter().min().unwrap();
+    let max_range = *crabs.iter().max().unwrap();
 
     let mut min_distance = sum_distance(&crabs, max_range);
     for i in min_range..max_range {
@@ -43,7 +23,29 @@ fn part_one(input: &str) -> i32 {
 }
 
 fn part_two(input: &str) -> i32 {
-    0
+    let crabs: Vec<i32> = input.split(",").map(|n| n.parse().unwrap()).collect();
+    let min_range = *crabs.iter().min().unwrap();
+    let max_range = *crabs.iter().max().unwrap();
+
+    let mut min_distance = i32::MAX;
+    let mut memoize = vec![0; (max_range + 1) as usize];
+    memoize[0] = 0;
+    memoize[1] = 1;
+    for i in 2..=max_range {
+        memoize[i as usize] = i + memoize[(i - 1) as usize];
+    }
+
+    for i in min_range..=max_range {
+        let mut current_sum = 0;
+        for x in &crabs {
+            let distance = (x - i).abs();
+            current_sum += memoize[distance as usize];
+        }
+        if current_sum < min_distance {
+            min_distance = current_sum;
+        }
+    }
+    min_distance
 }
 
 fn main() {
@@ -65,6 +67,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = include_str!("day7-example.txt");
-        assert_eq!(part_two(input), 0);
+        assert_eq!(part_two(input), 168);
     }
 }

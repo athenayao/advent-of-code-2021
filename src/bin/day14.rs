661e973e490fcb567174a2acbab1fcd8bea10edd
Athena Yao <athena@discordapp.com>
2021-12-14 00:32:36 -0800

    [day 14] Part 2

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -13,6 +13,29 @@ fn insert(template: &Vec<String>, rules: &HashMap<String, String>) -> Vec<String
     }
     modified
 }
+
+fn f(first: &str, second: &str) -> String {
+    format!("{}{}", first, second)
+}
+fn insert_with_counters(pair_counters: &mut HashMap<String, u64>, rules: &HashMap<String, String>) {
+    let mut to_add = HashMap::new();
+    let mut to_remove = HashMap::new();
+    for (pair, insert) in rules {
+        if let Some(count) = pair_counters.get(pair) {
+            *to_add.entry(f(&pair[..1], insert)).or_insert(0) += count;
+            *to_add.entry(f(insert, &pair[1..])).or_insert(0) += count;
+            *to_remove.entry(pair.clone()).or_insert(0) += count;
+        }
+    }
+
+    for (pair, count) in to_add {
+        *pair_counters.entry(pair).or_insert(0) += count;
+    }
+    for (pair, count) in to_remove {
+        *pair_counters.entry(pair).or_insert(0) -= count;
+    }
+}
+
 fn part_one(input: &str) -> u32 {
     let mut lines = input.split("\n");
 
@@ -47,13 +70,54 @@ fn part_one(input: &str) -> u32 {
     counts.values().max().unwrap() - counts.values().min().unwrap()
 }
 
-fn part_two(input: &str) -> u32 {
-    0
+fn part_two(input: &str) -> u64 {
+    let mut lines = input.split("\n");
+
+    let mut pair_counters = HashMap::new();
+    let mut last_letter = String::from("");
+    if let Some(line) = lines.next() {
+        let start: Vec<String> = line.chars().map(|c| c.to_string()).collect();
+        last_letter = start.last().unwrap().to_owned();
+        for i in 0..start.len() - 1 {
+            *pair_counters
+                .entry(f(&start[i], &start[i + 1]))
+                .or_insert(0) += 1;
+        }
+    }
+
+    let _ = lines.next();
+
+    let mut rules = HashMap::new();
+    loop {
+        if let Some(line) = lines.next() {
+            let line: Vec<&str> = line.split(" -> ").collect();
+            let from = line[0];
+            let to = line[1];
+            rules.insert(from.to_owned(), to.to_owned());
+        } else {
+            break;
+        }
+    }
+
+    for _ in 0..40 {
+        insert_with_counters(&mut pair_counters, &rules);
+    }
+
+    let mut element_counters = HashMap::new();
+    element_counters.insert(last_letter.to_owned(), 1);
+    for (pair, count) in pair_counters {
+        *element_counters
+            .entry(pair[0..1].to_owned()) // first character of every pair
+            .or_insert(0) += count;
+    }
+
+    // dbg!("{}", &element_counters);
+    element_counters.values().max().unwrap() - element_counters.values().min().unwrap()
 }
 
 fn main() {
     let input = include_str!("day14.txt");
-    println!("Part 1: {}", part_one(input));
+    // println!("Part 1: {}", part_one(input));
     println!("Part 2: {}", part_two(input));
 }
 
@@ -70,6 +134,17 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = include_str!("day14-example.txt");
-        assert_eq!(part_two(input), 0);
+        assert_eq!(part_two(input), 1588);
     }
+}
+
+fn count_pairs(line: &str) {
+    let mut pair_counters = HashMap::new();
+    let start: Vec<String> = line.chars().map(|c| c.to_string()).collect();
+    for i in 0..start.len() - 1 {
+        *pair_counters
+            .entry(f(&start[i], &start[i + 1]))
+            .or_insert(0) += 1;
+    }
+    // dbg!("COUNT_PAIRS", pair_counters);
 }
